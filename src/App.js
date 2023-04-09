9031a97beef6df2869bb59183b1f7630c5802cfa
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-04-09 13:39:36 +0530

    Changes commited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './styles/App.css';
 import Hero from './components/hero';
+import About from './components/About';
 function App() {
   return (
     <div className="App">
       <div className='hero-section'>
         <Hero />
+      </div>
+      <div className='about-section'>
+        <About />
       </div>
     </div>
   );
