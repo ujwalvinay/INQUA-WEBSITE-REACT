2ccc814c11b29b1058e8f6e23b54f2dc6336aeeb
Ujwal Vinay <walkerdoodle7@gmail.com>
2023-04-09 14:58:42 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import Hero from './components/hero';
 import About from './components/About';
+import ProShows from './components/proshow';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,9 @@ function App() {
       </div>
       <div className='about-section'>
         <About />
+      </div>
+      <div className='proshow-section'>
+        <ProShows />
       </div>
     </div>
   );
